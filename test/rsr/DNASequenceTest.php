fbcb34da84b30cb6ce42fce4720c02a378923762
Sundays <sundays@wagn.me>
2017-10-20 22:58:23 +0100

    Remove test that could cause phpunit to fail due to the random nature of silent mutaitons.

--- test/rsr/DNASequenceTest.php
+++ test/rsr/DNASequenceTest.php
@@ -22,21 +22,4 @@ class DNASequenceTest extends TestCase
         $result = $dna->getResult();
         $this->assertEquals('ATTAATAGGTATCCGGGATATATAT', $result->raw);
     }
-
-    /**
-     * @covers DNAResult
-     * @covers DNASequence::findRestrictionSites
-     */
-    public function testCorrectMutation()
-    {
-        $dnaSequence = 'ATATGGGCCCATGCC';
-        $formattedExpected = "ATATGG<span class=\"bg-primary\">GCT</span>CATGCC";
-        $dna = new DNASequence($dnaSequence);
-        $sites = array(RestrictionSites::getSite("GGGCCC"));
-
-        $dna->findRestrictionSites($sites);
-        $result = $dna->getResult();
-        $this->assertEquals('ATATGGGCTCATGCC', $result->raw);
-        $this->assertEquals($formattedExpected, $result->formatted);
-    }
 }
